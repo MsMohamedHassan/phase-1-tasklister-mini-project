6fe4361e6cef732c470e2e6b3c9af5bdf8ef4a75
Zamzam <Zamzamhassan44@gmail.com>
2022-06-14 23:08:44 +0300

    tasklister-mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+
+
+const form = document.getElementById("create-task-form");
+const inputValue = document.querySelector('#new-task-description');
+const toDoList = document.getElementById('task');
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  form.addEventListener('submit', newTask);
 });
+
+
+const newTask = (e) => {
+  e.preventDefault();
+  
+  let toDoList = document.getElementById('new-task-description').value;
+  newAppendedTask(toDoList)
+  e.target.reset()
+
+
+};
+const newAppendedTask = (task) =>{
+
+  let li = document.createElement('li')
+  li.textContent = task
+  document.getElementById('task').appendChild(li)
+}
+  
